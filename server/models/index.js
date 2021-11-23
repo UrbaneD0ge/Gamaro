a4e8f0fa754537d7cebd2aa0ef735029d87a2a39
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-11-23 13:17:17 -0500

    added variables and exports to index

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require("./User");
+const Product = require("./Product");
+const Category = require("./Category");
+const Inventory = require("./Inventory");
+const Order = require("./Order");
+
+module.exports = { User, Product, Category, Inventory, Order };
