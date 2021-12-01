c2c9db8b1ad486eb0757a339d2f60853d0d35023
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-11-30 23:06:34 -0500

    put a bandaid on the modal booboos

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -41,21 +41,27 @@ export const LOGIN = gql`
 `;
 
 // add product
-// export const ADD_PRODUCT = gql`
-// mutation addProduct($name: String!, $description: String!, $image: String!, $condition: String!, $price: Number!, $quantity: Number!) {
-//   addProduct(
-//     name: $name,
-//     description: $description,
-//     image: $image,
-//     condition: $condition,
-//     price: $price,
-//     quantity: $quantity
-//     ) {
-//       category {
-//         name: $category,
-//         gameOrConsole: $gameOrConsole
-
-//       }
-//     }
-// }
-// `;
+export const ADD_PRODUCT = gql`
+  mutation addProduct(
+    $name: String!
+    $description: String!
+    $image: String!
+    $condition: String!
+    $price: Number!
+    $quantity: Number!
+  ) {
+    addProduct(
+      name: name
+      description: description
+      image: image
+      condition: condition
+      price: price
+      quantity: quantity
+    ) {
+      category {
+        name: category
+        gameOrConsole: gameOrConsole
+      }
+    }
+  }
+`;
